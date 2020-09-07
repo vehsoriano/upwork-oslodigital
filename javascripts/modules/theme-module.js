d9f4021d9c23f5a492f299396745318ae36e13db
ariannajguiao <ariannajgguiao@gmail.com>
2020-09-07 17:53:44 +0800

    add html markup

--- javascripts/modules/theme-module.js
+++ javascripts/modules/theme-module.js
@@ -1,37 +1,43 @@
 AppName.Modules.ThemeModule = (function () {
-    //Dependencies
-    var core = AppName.Core;
-  
-    //////////////////////
-    // Private Methods //
-    ////////////////////
-    var _privateMethod = function () {
-      // private stuff
-    };
-  
-    /////////////////////
-    // Public Methods //
-    ///////////////////
-    var init = function () {
-      _privateMethod();
-    };
-  
-    return {
-      init: init,
-    };
-  })();
-  
-  new fullpage("#fullpage", {
-    licenseKey: "YOUR KEY HERE",
-    // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  //Dependencies
+  var core = AppName.Core;
+
+  //////////////////////
+  // Private Methods //
+  ////////////////////
+  var _privateMethod = function () {
+    // private stuff
+  };
+
+  /////////////////////
+  // Public Methods //
+  ///////////////////
+  var init = function () {
+    _privateMethod();
+  };
+
+  return {
+    init: init,
+  };
+})();
+
+new fullpage("#fullpage", {
+  licenseKey: "YOUR KEY HERE",
+  // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+});
+
+$(document).ready(function () {
+  console.log("ready!");
+  var swiper = new Swiper(".swiper-container.case-study", {
+    navigation: {
+      nextEl: ".custom-swiper-next",
+      prevEl: ".custom-swiper-prev",
+    },
   });
-  
-  $(document).ready(function () {
-    console.log("ready!");
-    var swiper = new Swiper(".swiper-container", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+
+  var swiperNewsletter = new Swiper(".swiper-container.newsletter", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
   });
+});
