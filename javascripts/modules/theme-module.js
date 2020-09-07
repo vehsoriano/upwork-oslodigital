615215d4775c8533f6b2a4f5873cd8684fe21a37
vehsoriano <verrell.soriano09@gmail.com>
2020-09-07 16:07:18 +1000

    updates

--- javascripts/modules/theme-module.js
+++ javascripts/modules/theme-module.js
@@ -23,7 +23,7 @@ AppName.Modules.ThemeModule = (function () {
 
 new fullpage("#fullpage", {
   licenseKey: "YOUR KEY HERE",
-  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
 });
 
 $(document).ready(function () {
