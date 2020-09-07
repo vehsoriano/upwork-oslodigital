f60b60dfa6f7a1a245fb5361f6a9bab56f1db1b5
ariannajguiao <ariannajgguiao@gmail.com>
2020-09-07 13:00:46 +0800

    add fullpage

--- javascripts/modules/theme-module.js
+++ javascripts/modules/theme-module.js
@@ -1,22 +1,27 @@
-AppName.Modules.ThemeModule = (function() {
+AppName.Modules.ThemeModule = (function () {
   //Dependencies
   var core = AppName.Core;
 
   //////////////////////
   // Private Methods //
   ////////////////////
-  var _privateMethod = function() {
+  var _privateMethod = function () {
     // private stuff
   };
 
   /////////////////////
   // Public Methods //
   ///////////////////
-  var init = function() {
+  var init = function () {
     _privateMethod();
   };
 
   return {
-    init: init
+    init: init,
   };
 })();
+
+new fullpage("#fullpage", {
+  licenseKey: "YOUR KEY HERE",
+  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+});
