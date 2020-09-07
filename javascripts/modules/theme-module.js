e6dffa3304a3c277e3e3e23d4ae32819a92f1d1d
ariannajguiao <ariannajgguiao@gmail.com>
2020-09-07 18:39:52 +0800

    add youtube modal

--- javascripts/modules/theme-module.js
+++ javascripts/modules/theme-module.js
@@ -40,4 +40,20 @@ $(document).ready(function () {
     spaceBetween: 30,
     loop: true,
   });
+
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  $("#myModal").on("shown.bs.modal", function (e) {
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+
+  $("#myModal").on("hide.bs.modal", function (e) {
+    $("#video").attr("src", $videoSrc);
+  });
 });
